e0791467ea868901fa12358cf62629feb33b25e1
11249A021 <11249a021@kanchiuniv.ac.in>
2026-07-22 11:10:44 +0530

    Add files via upload

---
+++ VariablesDemo.java
@@ -0,0 +1,17 @@
+public class VariablesDemo{
+int instanceVar=10;
+static String staticVar="l am static";
+public void showVariables()
+{
+int localVar=5;
+System.out.println("instance variable:"+instanceVar);
+System.out.println("static variable:"+staticVar);
+System.out.println("local variable:"+localVar);
+}
+public static void main(String[]args)
+{
+VariablesDemo obj1=new VariablesDemo();
+obj1.showVariables();
+System.out.println("accessing static variable via class:"+VariablesDemo.staticVar);
+}
+}
